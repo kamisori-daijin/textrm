3f533f99579b9874c7156e239e2529351037df3d
kamisori-daijin <208445078+kamisori-daijin@users.noreply.github.com>
2026-02-12 12:20:02 +0900

    refactor: move argument parsing and model loading to the top of the file

--- converter.py
+++ converter.py
@@ -5,6 +5,12 @@ import torch
 from models.trm_model import TinyRecursiveModel
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--model-path', type=str, required=True, help='Path to the model file to load')
+
+args = parser.parse_args()
+
+model = torch.load(args.model_path)
 
 def convert_model(self, model: TinyRecursiveModel) -> ct.models.MLModel:
     """Convert TRM model to CoreML format"""
@@ -20,12 +26,7 @@ def convert_model(self, model: TinyRecursiveModel) -> ct.models.MLModel:
     )
     return mlmodel
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--model-path', type=str, required=True, help='Path to the model file to load')
 
-args = parser.parse_args()
-
-model = torch.load(args.model_path)
 
 model.eval()
 print("Model loaded and set to eval mode.")
